35c38a0bde6832aea3ccd5214d83c4721794333a
XSNomad <60951221+XSNomad@users.noreply.github.com>
2023-05-20 04:12:55 -0400

    Init
    Init of BiggerCrewQuarters

---
+++ Plugin.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using BulwarkStudios.Stanford.Torus.Buildings.Actions;
+using BepInEx.Logging;
+
+namespace BiggerCrewQuarters;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    public override void Load()
+    {
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        var harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        harmony.PatchAll();
+
+        foreach (var patchedMethod in harmony.GetPatchedMethods())
+        {
+            Log.LogInfo($"Patched: {patchedMethod.DeclaringType?.FullName}:{patchedMethod}");
+        }
+    }
+}
+public class MoreCitizens
+{
+
+    [HarmonyPatch(typeof(BuildingActionBehaviourQuarter), nameof(BuildingActionBehaviourQuarter.GetMaxCitizen))]
+    public static class Quarter_Patch
+    {
+        public static void Postfix(ref int __result)
+        {
+            __result *= 2;
+        }
+    }
+    [HarmonyPatch(typeof(BuildingActionBehaviourHouse), nameof(BuildingActionBehaviourHouse.GetMaxCitizen))]
+    public static class House_Patch
+    {
+        public static void Postfix(ref int __result)
+        {
+            __result *= 2;
+        }
+    }
+    [HarmonyPatch(typeof(BuildingActionBehaviourCellHousing), nameof(BuildingActionBehaviourCellHousing.GetMaxCitizen))]
+    public static class CellHousing_Patch
+    {
+        public static void Postfix(ref int __result)
+        {
+            __result *= 2;
+        }
+    }
+}
